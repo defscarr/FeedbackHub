4ad1a1f253d70155d565df30b38ab0a30b9e59a1
defscarr <anon.aryan.admin@protonmail.com>
2023-06-04 18:25:41 +0000

    Update(UI): Add responsiveness to "Roadmap" page
    	- Add tablet responsiveness
    	- Add mobile responsiveness
    	- Add desktop responsiveness

--- app/board/roadmap.tsx
+++ app/board/roadmap.tsx
@@ -2,16 +2,16 @@ export default function Roadmap(){
 
     const GenerateCards = () =>{
         const cards: any[] = [];
-        for(let i = 0; i <= 8; i++){
+        for(let i = 1; i <= 8; i++){
             cards.push(
             <div className="flex flex-col p-8 bg-white border-t-4 border-xOrange-300 rounded-lg">
                 <div className="flex flex-row items-center space-x-4 mb-2">
                     <span className="h-2 w-2 rounded-full bg-xOrange-300"></span>
-                    <span className="flex-1 font-normal text-16x text-xSlate-500">Planned</span>
+                    <span className="flex-1 font-normal text-13x lg:text-16x text-xSlate-500">Planned</span>
                 </div>
                 <div className="mb-4">
-                    <h1 className="leading-26 tracking-closer font-bold text-18x text-xSlate-600 cursor-pointer hover:text-xIndigo-600">More comprehensive reports</h1>
-                    <p className="text-16x font-normal text-xSlate-500">It would be great to see a more detailed breakdown of solutions.</p>
+                    <h1 className="leading-26 tracking-closer font-bold text-13x lg:text-18x text-xSlate-600 cursor-pointer hover:text-xIndigo-600">More comprehensive reports</h1>
+                    <p className="text-13x lg:text-16x font-normal text-xSlate-500">It would be great to see a more detailed breakdown of solutions.</p>
                 </div>
                 <div className="flex flex-row items-center mb-4">
                     <span className="rounded-lg text-center bg-xSiolet-50">
@@ -20,7 +20,7 @@ export default function Roadmap(){
                 </div>
                 <div className="flex flex-row items-center">
                     <div className="flex-1 flex items-center">
-                        <span className="flex items-center cursor-pointer px-4 py-2.5 bg-xSiolet-50 rounded-xl hover:bg-indigo-200">
+                        <span className="flex items-center cursor-pointer px-4 py-1.5 lg:py-2.5 bg-xSiolet-50 rounded-xl hover:bg-indigo-200">
                             <span>
                                 <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 6l4-4 4 4" stroke="#4661E6" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
                             </span>
@@ -30,7 +30,7 @@ export default function Roadmap(){
 
                     <span className="flex space-x-2 items-center">
                         <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fill-rule="nonzero"/></svg>
-                        <h1 className="leading-26 tracking-closer font-bold text-xSlate-600 text-16x">4</h1>
+                        <h1 className="leading-26 tracking-closer font-bold text-xSlate-600 text-13x lg:text-16x">4</h1>
                     </span>
                 </div>
             </div>
@@ -41,8 +41,8 @@ export default function Roadmap(){
     }
 
     return (
-        <main className="flex flex-col px-40 pt-20 pb-20 space-y-12 h-screen w-screen">
-            <header className="flex flex-row bg-xSlate-700 px-8 py-4 rounded-xl">
+        <main className="flex flex-col pt-0 px-0 pb-24 md:pt-14 md:px-10 md:pb-28 lg:px-40 lg:pt-20 lg:pb-20 space-y-0 md:space-y-12 h-screen w-screen overflow-auto">
+            <header className="flex flex-row bg-xSlate-700 px-8 py-6 rounded-none md:rounded-xl">
                 <div className="flex-1 flex flex-col text-white">
                     <button className="flex items-center hover:underline hover:decoration-xSlate-500">
                         <span className="-mt-1">
@@ -60,12 +60,12 @@ export default function Roadmap(){
                 </button>
             </header>
 
-            <section className="flex-1 flex overflow-hidden space-x-8">
+            <section className="sm:hidden flex-1 md:flex overflow-hidden space-x-8">
 
                 <div className="flex flex-col w-1/3">
                     <span className="mb-8 relative">
-                        <h1 className="font-bold text-18x leading-26 tracking-closer text-xSlate-600">Planned (4)</h1>
-                        <p className="font-normal text-16x text-xSlate-500">Ideas prioritized for research</p>
+                        <h1 className="font-bold text-14x lg:text-18x leading-26 tracking-closer text-xSlate-600">Planned (4)</h1>
+                        <p className="font-normal text-14x lg:text-16x text-xSlate-500">Ideas prioritized for research</p>
                     </span>
 
                     <div className="flex flex-col space-y-6 overflow-y-auto">
@@ -75,8 +75,8 @@ export default function Roadmap(){
 
                 <div className="flex flex-col  w-1/3">
                     <span className="mb-8 relative">
-                        <h1 className="font-bold text-18x leading-26 tracking-closer text-xSlate-600">In-Progress (3)</h1>
-                        <p className="font-normal text-16x text-xSlate-500">Currently being developed</p>
+                        <h1 className="font-bold text-14x lg:text-18x leading-26 tracking-closer text-xSlate-600">In-Progress (3)</h1>
+                        <p className="font-normal text-14x lg:text-16x text-xSlate-500">Currently being developed</p>
                     </span>
 
                     <div className="flex flex-col space-y-6  overflow-y-auto">
@@ -86,8 +86,8 @@ export default function Roadmap(){
 
                 <div className="w-1/3 flex flex-col">
                     <span className="mb-8">
-                        <h1 className="font-bold text-18x leading-26 tracking-closer text-xSlate-600">Live (1)</h1>
-                        <p className="font-normal text-16x text-xSlate-500">Released features</p>
+                        <h1 className="font-bold text-14x lg:text-18x leading-26 tracking-closer text-xSlate-600">Live (1)</h1>
+                        <p className="font-normal text-14x lg:text-16x text-xSlate-500">Released features</p>
                     </span>
 
                     <div className="flex flex-col space-y-6 overflow-y-auto">
@@ -96,6 +96,38 @@ export default function Roadmap(){
                 </div>
 
             </section>
+
+
+            <nav className="flex border-b md:hidden">
+                <ul className="flex-1 flex text-13x text-xSlate-500 font-bold tracking-close">
+                    <li className="flex-1 hover:border-b-2 py-5 border-xFuchisia-600 hover:text-xSlate-600">
+                        <h5 className="text-center">Planned (4)</h5>
+                    </li>
+                    <li className="flex-1 hover:border-b-2 py-5 border-xFuchisia-600 hover:text-xSlate-600">
+                        <h5 className="text-center">In-Progress (2)</h5>
+                    </li>
+                    <li className="flex-1 hover:border-b-2 py-5 border-xFuchisia-600 hover:text-xSlate-600">
+                        <h5 className="text-center">Live (4)</h5>
+                    </li>
+                    
+                </ul>
+            </nav>
+
+
+            <div className="flex px-6 pt-6 flex-col md:hidden overflow-hidden">
+                <span className="relative mb-8">
+                    <h1 className="font-bold text-18x leading-26 tracking-closer text-xSlate-600">Live (1)</h1>
+                    <p className="font-normal text-16x text-xSlate-500">Released features</p>
+                </span>
+
+                <div className="flex flex-col space-y-6 overflow-y-auto">
+                    {GenerateCards()}
+                </div>
+            </div>
+
+
+
+
         </main>
 
     );
